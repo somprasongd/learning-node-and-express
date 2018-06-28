f9e46e9511bad8898b88409db092d5ee0ccb1d2d
Somprasong Damyos <somprasong@hospital-os.com>
2018-06-28 09:17:17 +0700

    fix merge app.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,28 @@
 import express from 'express';
-<<<<<<< HEAD
 import PetRoute from './routes/pets';
-=======
 import morgan from 'morgan';
-import pets from './../data/pets.json';
->>>>>>> step-009-express-middleware-part-2
 
 const PORT = 3000;
 
 const server = express();
 
-<<<<<<< HEAD
+// setup routes path
 const buildUrl = (version, path) => `/api/${version}/${path}`;
 const PETS_BASE_URL = buildUrl('v1', 'pets');
 
-server.use((req, res, next) => {
-	const now = new Date().toString();
-	const log = `${now}: ${req.method} ${req.url}`;
-	console.log(log);
-	next();
-});
-=======
+// use middlewares
 server.use(morgan('tiny'));
->>>>>>> step-009-express-middleware-part-2
 
+// default route
 server.get('/', (req, res) => {
   console.log('handling GET request...');
   res.send('Hello from Express');
 });
 
+// routes with router
 server.use(PETS_BASE_URL, PetRoute);
 
+// start server
 server.listen(PORT, () => {
   console.log(`Server start on port ${PORT}`)
 });
